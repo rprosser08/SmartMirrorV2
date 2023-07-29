0824666d9fd966c7ede8a350c7abbc6feaa8016c
Richard Prosser <rdprosser@icloud.com>
2023-07-28 23:47:42 -0400

    Refactored date and time labels and updating

--- mainframe.py
+++ mainframe.py
@@ -8,19 +8,32 @@ class MainFrame:
         self.root.title("Smart Mirror")
         self.root.configure(background="black")
         self.root.attributes("-fullscreen", True)
+
+        # Date Label
+        self.date_text = Label(self.root)
+        self.date_text.pack()
         self.get_date()
+
+        # Time Label
+        self.time_text = Label(self.root)
+        self.time_text.pack()
         self.get_time()
+
+
         self.root.mainloop()
 
     
+    # Responsible for updating the current date every 500ms
     def get_date(self):
-        date_text = Label(self.root, text=Date.get_date())
-        date_text.pack()
+        self.date_text.configure(text=Date.get_date())
+        self.date_text.after(500, self.get_date)
 
     
+    # Responsible for updating the current time every 500ms
     def get_time(self):
-        time_text = Label(self.root, text=Date.get_time())
-        time_text.pack()
+        self.time_text.configure(text=Date.get_time())
+        self.time_text.after(500, self.get_time)
+
 
 
 
